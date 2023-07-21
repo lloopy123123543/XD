261f094672f6622986cb43809c1ed7632bb07ce4
Igor Parfenov <lloopy123123543@gmail.com>
2023-07-21 17:47:56 +0500

    supabase start

--- resume/app/layout.js
+++ resume/app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Figtree} from 'next/font/google'
 import Header from './components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create your resume',
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
         <Header></Header>
         {children}
         </body>
